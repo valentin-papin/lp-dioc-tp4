18de884538f900fad954f7a015362d1b50fe7165
valentin-papin <valentin.papin1@gmail.com>
2017-11-15 11:34:14 +0100

    Draft3

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,11 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\UserType;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use App\Entity\User;
 
 class SecurityController extends Controller
 {
@@ -37,5 +40,26 @@ class SecurityController extends Controller
     {
         // FIXME: Instancier le formulaire et à la soumission enregistrer le user.
         // La vue à rendre : Security/register.html.twig
+        // create a task and give it some dummy data for this example
+        $em = $this->getDoctrine()->getManager();
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('firstname')
+            ->add('lastname')
+            ->add("email")
+            ->add('birthday')
+            ->add('password')
+            ->add('save', SubmitType::class, array('label' => 'Create User'))
+            ->getForm();
+        $form = $this->createForm(UserType::class, $user);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            //$user->setCreatedAt(new \Datetime('now'));
+            $em->persist($user);
+            $em->flush();
+        }
+
+        return $this->render("Security/register.html.twig", array('form' => $form->createView()));
     }
 }
